8ca5ff3f2d887a19372d7643c0861be00f5d450d
AlejandroHV <alejandro.herreno@hotmail.com>
2015-07-04 17:40:19 -0500

    Adding DbContext

--- EasyTourney/Models/DBEasyTourney.Designer.cs
+++ EasyTourney/Models/DBEasyTourney.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\alejandro\documents\visual studio 2013\Projects\EasyTourney\EasyTourney\Models\DBEasyTourney.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Alejandro\Documents\Visual Studio 2013\Projects\EasyTourney\EasyTourney\Models\DBEasyTourney.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
