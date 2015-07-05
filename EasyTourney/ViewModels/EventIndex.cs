5305c9570ee9802ac6e6951f3edaa84c6c28dec8
AlejandroHV <alejandro.herreno@hotmail.com>
2015-07-05 01:47:51 -0500

    EventIndex

---
+++ EasyTourney/ViewModels/EventIndex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EasyTourney.Models;
+
+namespace EasyTourney.ViewModels
+{
+    public class EventIndex
+    {
+
+        public tblEvent eventInfo { get; set; }
+
+
+        public List<tblUserEvents> eventParticipants { get; set; }
+
+
+
+    }
+}
